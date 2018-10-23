eceeac3ac2acfadbb88371758b02958a89610005
CInergy92 <44379683+CInergy92@users.noreply.github.com>
2018-10-22 21:52:13 -0400

    Update NFS Server Script.rb

--- Project2/NFS Server Script.rb
+++ Project2/NFS Server Script.rb
@@ -1,55 +1,74 @@
-#!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-	exit 1
-fi
-yum install ruby
-#!/usr/bin/ruby
+#!/usr/bin/ruby -w
 #NFS Server Install Script
-`cp -R /home /tmp/oldhome`
+require 'open3'
+`sudo cp -R /home /tmp/oldhome`
 def addPart()
-	`fdisk /dev/sda`
-	`n`
-	`p`
-	`"\n"`
-	`+4096M`
-	`w`
-	`partprobe /dev/sda`
-	`mkfs.xfs /dev/sda4`
-	`xfs_repair /dev/sda`
-	`mount /dev/sda4 /home`
-	`umount /home`
-	open('/etc/fstab', 'a') do |f|
-		f.puts "/dev/sda4  /home  xfs  defaults 0 0"
-	end
-	`mount /home`
-	`mv -R /tmp/oldhome/* /home`
+        puts "start Adding partition"
+        Open3.popen2('sudo fdisk /dev/sda') { |i,o,t|
+                i.puts 'n'
+                i.puts 'p'
+                i.puts ''
+                i.puts '+4096M'
+                i.puts 'w'
+                i.close
+                p o.read
+                t.join
+        }
+
+        `sudo partprobe /dev/sda`
+        `sudo mkfs.xfs -f /dev/sda4`
+        `sudo xfs_repair /dev/sda4`
+        `sudo mount /dev/sda4 /home`
+        `sudo umount /home`
+        open('/etc/fstab', 'a') do |f|
+                f.puts "/dev/sda4  /home  xfs  defaults 0 0"
+        end
+        `sudo mount /home`
+        `sudo mv /tmp/oldhome/home/* /home`
+        puts "End adding partition"
 end
 def editExports()
-	open('/etc/exports', 'w') do |f|
-		f.puts "/home 10.2.6.*(rw)"
-	end
+        puts "start editing exports"
+        open('/etc/exports', 'w') do |f|
+                f.puts "/home 10.2.6.*(rw)"
+        end
+        puts "End  editing exports"
 end
 def nfsSetup()
-	`chmod -R 755 /home`
-	`yum install nfs-utils`
-	`systemctl enable rpcbind`
-	`systemctl enable nfs-server`
-	`systemctl enable nfs-lock`
-	`systemctl enable nfs-idmap`
-	`systemctl start rpcbind`
-	`systemctl start nfs-server`
-	`systemctl start nfs-lock`
-	`systemctl start nfs-idmap`
-	`exportfs -a`
-	`systemctl restart nfs-server`
-	`firewall-cmd --zone=public --add-port=2049/tcp --permanent`
-	`firewall-cmd --zone=public --add-port=111/tcp --permanent`
-	`firewall-cmd --zone=public --add-port=20048/tcp --permanent`
-	`firewall-cmd --zone=public --add-port=2049/udp --permanent`
-	`firewall-cmd --zone=public --add-port=111/udp --permanent`
-	`firewall-cmd --zone=public --add-port=20048/udp --permanent`
-	`firewall-cmd --reload`
+        puts "Start nfs Setup"
+        `sudo chmod -R 755 /home`
+        puts "Installing nfs-utils"
+        `sudo yum -y install nfs-utils`
+        puts "install finished"
+        puts "enabling nfs"
+        `sudo systemctl enable rpcbind`
+        `sudo systemctl enable nfs-server`
+        `sudo systemctl enable nfs-lock`
+        `sudo systemctl enable nfs-idmap`
+        puts "starting services"
+        `sudo systemctl start rpcbind`
+        `sudo systemctl start nfs-server`
+        `sudo systemctl start nfs-lock`
+        `sudo systemctl start nfs-idmap`
+        puts "exporting NFS"
+        `sudo exportfs -a`
+        puts "restarting NFS"
+        puts "ending nfs setup"
+end
+def editFirewall()
+        puts "Editing the firewall"
+        `sudo systemctl restart nfs-server`
+        `sudo firewall-cmd --zone=public --add-port=2049/tcp --permanent`
+        `sudo firewall-cmd --zone=public --add-port=111/tcp --permanent`
+        `sudo firewall-cmd --zone=public --add-port=20048/tcp --permanent`
+        `sudo firewall-cmd --zone=public --add-port=2049/udp --permanent`
+        `sudo firewall-cmd --zone=public --add-port=111/udp --permanent`
+        `sudo firewall-cmd --zone=public --add-port=20048/udp --permanent`
+        `sudo firewall-cmd --reload`
+	puts "firewall edited"
 end
 addPart()
 editExports()
 nfsSetup()
+editFirewall()
+print "NFS Setup complete"
