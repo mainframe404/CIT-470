29db917f18d7c217adc5d8e0c255447982925401
LongHairedEagle <37786111+LongHairedEagle@users.noreply.github.com>
2018-12-04 02:11:57 -0500

    Update Monit-Testing.sh

--- Project-3/Monit-Testing.sh
+++ Project-3/Monit-Testing.sh
@@ -9,7 +9,7 @@ function ClientTest {
   
   #Kill SSH
   echo -e "Stopping The SSH Process"
-  #pkill sshd
+  pkill sshd
 
   #Kill LDAP
   echo -e "Stopping LDAP"
@@ -22,16 +22,15 @@ function ClientTest {
   #Kill syslog process
   echo -e "Stopping The Syslog Process"
   pkill rsyslog
-  echo -e "Testing takes 5 seconds to collect logs. Please wait... "
-  sleep 5s
+  
   tail -n 15 /var/log/messages
   echo -e "Monit Sended The  Alerts and Restarted The Services"
 
   #Test The  RAM, CPU, and Disk Usage
   echo -e "Testing RAM, CPU, and Disk Usage"
-  echo -e "Testing takes Few minutes To Collect  The Logs. Please wait..."
+  echo -e "Testing takes a few minutes. Please wait..."
   stress -c 8 -d 1 --hdd-bytes 5.5G --vm-bytes $(awk '/MemFree/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo)k --vm-keep -m 1 -t 200s
-  sleep 5s
+
   tail -n 15 /var/log/messages
   echo -e "Test Successfull."
  
